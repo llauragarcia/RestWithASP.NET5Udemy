21dfbc6efd3d5c7e1714581ccc2bcea79828ae2d
Laura Garcia <lauragbio@outlook.com>
2021-09-21 19:43:34 -0300

    Configuraçao da API

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,71 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+            return people;
+        }
+
+        public Person FindByID(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Laura",
+                LastName = "Garcia",
+                Address = "Belo Horizonte - Minas Gerais - Brasil",
+                Gender = "Female"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + 1,
+                LastName = "Person LastName" + 1,
+                Address = "Some Address" + 1,
+                Gender = "Male"
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
